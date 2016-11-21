be9d06c04c2c5792dc70f306a73df71820023b52
JonnaJudin <jonna.judin@gmail.com>
2016-11-21 14:39:21 +0000

    added Program.cs which takes unspecified number of command line arguments and prints them back to the user. Prints usage information if no arguments given.

---
+++ Tutorial1/Tutorial1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tutorial1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length < 1){
+                Console.WriteLine("Usage: {0} <1> [2] .. [n] ", System.AppDomain.CurrentDomain.FriendlyName);
+                Console.WriteLine("Prints out argument(s) 1(..n)");
+            }
+            else {
+                Console.WriteLine("You gave {0} argument(s):",args.Length);
+                for(int i = 0; i < args.Length; i++){
+                    Console.WriteLine("Argument {0}: {1}",i+1,args[i]);
+                };
+            };
+        }
+    }
+}
