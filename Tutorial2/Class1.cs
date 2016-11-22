d875e7d613beb0680bcb1a168ffaca0c1014913e
JonnaJudin <jonna.judin@gmail.com>
2016-11-22 14:29:31 +0000

    hacky solution to SPOJ task 1

---
+++ Tutorial2/Class1.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+public class Tutorial2
+{
+	static void main (string[] args)
+	{
+        Console.WriteLine("Hi Again!");
+	}
+}
---
+++ Tutorial2/Tutorial2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tutorial2
+{
+    class Spoj1
+    {
+        static void Main(string[] args)
+        {
+            // Not great programming, but the problem description does not
+            // require us to cast them as numbers, just to compare if it's "42"
+
+            string rawInput = "";
+            rawInput = Console.ReadLine();
+            while (!rawInput.Equals("42"))
+            {
+                Console.WriteLine("You Typed: {0}", rawInput);
+                rawInput=Console.ReadLine();
+            };
+        }
+    }
+}
